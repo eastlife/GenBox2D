412b6d3060223cb6fea938dd43d43f722960aedc
eastlife <dengbx1212@163.com>
2020-07-01 02:31:23 -0400

    generate gif

--- src/main/python/visualization/visualizer.py
+++ src/main/python/visualization/visualizer.py
@@ -36,14 +36,19 @@ class Visualizer:
     def replay(self):
         timestamp_info = self.timestamp_info
         self.draw_single_picture("initial")
+        image_arr = []
         for timestamp, info in enumerate(timestamp_info):
             featurized_objects = self.featurized_objects
             for idx, body in enumerate(info['body_info']['bodies']):
                 featurized_objects[idx].initial_x = (body['pos_x'] + 10) / 20.0
                 featurized_objects[idx].initial_y = body['pos_y'] / 20.0
                 featurized_objects[idx].initial_angle = body['angle']
-            print(featurized_objects[0].initial_x, featurized_objects[idx].initial_y)
-            self.draw_single_picture(str(timestamp))
+            # print(featurized_objects[0].initial_x, featurized_objects[idx].initial_y)
+            image = self.draw_single_picture(str(timestamp))
+            image_arr.append(image)
+
+        image_arr[0].save('images/out.gif', save_all=True, append_images=image_arr)
+
 
 
     def draw_single_picture(self, name):
@@ -54,3 +59,4 @@ class Visualizer:
 
         image = image.transpose(Image.FLIP_TOP_BOTTOM)
         image.save("images/" + name + ".jpg")
+        return image
