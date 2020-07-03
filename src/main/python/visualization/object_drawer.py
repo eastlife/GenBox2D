8fc71d82cd93ba3827604f2d37e4d61680d20be4
eastlife <dengbx1212@163.com>
2020-07-02 21:19:53 -0400

    draw jar

--- src/main/python/visualization/object_drawer.py
+++ src/main/python/visualization/object_drawer.py
@@ -71,63 +71,100 @@ def draw_bar(draw, scene_width, scene_height, shape, color, diameter, x, y, angl
     draw.polygon(rectangle_vertices, fill=color)
 
 def draw_jar(draw, scene_width, scene_height, shape, color, diameter, x, y, angle):
-    scaled_diameter = diameter_percent_to_length(scene_width, diameter)
-    scaled_literal_offset = 0.02 * scaled_diameter
-    scaled_thickness = diameter_percent_to_length(scene_width, 0.005)
+    jar_height = 220 * diameter
+    jar_width = 160 * diameter
+    jar_base_width = 140 * diameter
+    jar_thickness = 4
+    vertices_list, _ = _build_jar_vertices(height=jar_height, width=jar_width, base_width=jar_base_width, thickness=jar_thickness)
+    jar_center = (x * scene_width, y * scene_height - jar_height / 2 + jar_thickness * 2)
+    for rect in vertices_list:
+        draw_polygon(draw, scene_width, scene_height, "jar", "red", 1.0, jar_center[0], jar_center[1], x * scene_width, y * scene_height, angle, rect)
 
-    literal_length_adjust = 0.75 * scaled_diameter
-    literal_angle = 0.0
-    literal_x_adjust = 3 * literal_length_adjust * literal_angle
 
-    bottom_length_adjust = 0.6 * scaled_diameter
-    bottom_y_adjust = 0.15 * scaled_diameter
+def draw_polygon(draw, scene_width, scene_height, shape, color, diameter, pos_x, pos_y, rotate_x, rotate_y, angle, vertices):
 
-    center_y_adjust = 1.5 * scaled_thickness
-    center = (width_percent_to_x(scene_width, x), height_percent_to_y(scene_height, y) - center_y_adjust)
+    # rectangle_center = (x * scene_width, y * scene_height)
+    rectangle_width = 2
+    rectangle_length = scene_width * diameter
+    rectangle_angle = 360 * angle
 
-    # literal 1 left
-    draw_rectangle(draw, scene_width, scene_height, shape, color, diameter, x, y, angle + 0.25 - 0.01, #(math.pi/2 - literal_angle) * 180 / math.pi, 
-                                    offset = (scaled_diameter / 2, -scaled_diameter / 2))
-                                    # offset = (diameter_percent_to_length(scene_width, scaled_literal_offset) - literal_x_adjust, 
-                                    # diameter_percent_to_length(scene_width, scaled_literal_offset * 0.5)))
-    print(scaled_diameter)
-    print(scaled_literal_offset)
-    print(diameter_percent_to_length(scene_width, scaled_literal_offset))
-    print(literal_x_adjust)
-    print((diameter_percent_to_length(scene_width, scaled_literal_offset) - literal_x_adjust, 
-                                    diameter_percent_to_length(scene_width, scaled_literal_offset * 0.5)))
+    rectangle_vertices = (
+        (vertices[0][0] + pos_x, vertices[0][1] + pos_y),
+        (vertices[1][0] + pos_x, vertices[1][1] + pos_y),
+        (vertices[2][0] + pos_x, vertices[2][1] + pos_y),  
+        (vertices[3][0] + pos_x, vertices[3][1] + pos_y)
+    )
 
-    # literal 2 right
-    draw_rectangle(draw, scene_width, scene_height, shape, color, diameter, x, y, angle + 0.25 + 0.01, #(math.pi/2 + literal_angle) * 180 / math.pi, 
-                                    offset = (scaled_diameter / 2, scaled_diameter / 2))
-                                    # offset = (diameter_percent_to_length(scene_width, scaled_literal_offset) + literal_x_adjust, 
-                                    # diameter_percent_to_length(scene_width, scaled_literal_offset * 0.5)))
+    rotate_center = (
+        rotate_x,
+        rotate_y
+    )
 
-    # bottom
-    draw_rectangle(draw, scene_width, scene_height, shape, color, diameter, x, y, angle, 
-                                    offset = (0, 0))
-                                    # offset = (diameter_percent_to_length(scene_width, 0), 
-                                    # diameter_percent_to_length(scene_width, 0) - bottom_y_adjust))
-    # literal1_shape = b2PolygonShape()
-    # literal1_shape.SetAsBox(literal_length_adjust / 2, # hx
-    #                         scaled_thickness, # hy
-    #                         b2Vec2(diameter_percent_to_length(scene_width, scaled_literal_offset) - literal_x_adjust, 
-    #                                 diameter_percent_to_length(scene_width, scaled_literal_offset * 0.5)), # offset 
-    #                         math.pi/2 - literal_angle) # angle
-    # # literal1_shape.position.set(b2Vec2(0.1, 0.1))
-    # literal2_shape = b2PolygonShape()
-    # literal2_shape.SetAsBox(literal_length_adjust / 2, 
-    #                         scaled_thickness, 
-    #                         b2Vec2(diameter_percent_to_length(scene_width, - scaled_literal_offset) + literal_x_adjust, 
-    #                                 diameter_percent_to_length(scene_width, scaled_literal_offset * 0.5)), 
-    #                         math.pi/2 + literal_angle)
-    # # literal1_shape.pos.set(b2Vec2(-0.1, 0.1))
-    # bottom_shape = b2PolygonShape()
-    # bottom_shape.SetAsBox(bottom_length_adjust / 2, 
-    #                         scaled_thickness, 
-    #                         b2Vec2(diameter_percent_to_length(scene_width, 0), 
-    #                                 diameter_percent_to_length(scene_width, 0) - bottom_y_adjust), 
-    #                         0)
+    rectangle_vertices = [rotated_about(x, y, rotate_center[0], rotate_center[1], math.radians(rectangle_angle)) for x,y in rectangle_vertices]
+    draw.polygon(rectangle_vertices, fill=color)
+
+def _build_jar_vertices(height, width, thickness, base_width):
+        # Create box.
+        vertices = []
+        for i in range(4):
+            vx = (1 - 2 * (i in (1, 2))) / 2. * base_width
+            vy = (1 - 2 * (i in (2, 3))) / 2. * thickness
+            vertices.append((vx, vy))
+
+        # Compute offsets for jar edge coordinates.
+        base = (width - base_width) / 2.
+        hypotenuse = math.sqrt(height**2 + base**2)
+        cos, sin = base / hypotenuse, height / hypotenuse
+        x_delta = thickness * sin
+        x_delta_top = thickness / sin
+        y_delta = thickness * cos
+
+        # Left tilted edge of jar.
+        vertices_left = [
+            (-width / 2, height - (thickness / 2)),
+            ((-base_width / 2), -(thickness / 2)),
+            ((-base_width / 2) + x_delta, y_delta - (thickness / 2)),
+            ((-width / 2) + x_delta_top, height - (thickness / 2)),
+        ]
+
+        # Right tilted edge.
+        vertices_right = [
+            (width / 2, height - (thickness / 2)),
+            ((width / 2) - x_delta_top, height - (thickness / 2)),
+            ((base_width / 2) - x_delta, y_delta - (thickness / 2)),
+            ((base_width / 2), -(thickness / 2)),
+        ]
+
+        phantom_vertices = (vertices_left[0], vertices_left[1],
+                            vertices_right[3], vertices_right[0])
+   
+        return [vertices, vertices_left, vertices_right], phantom_vertices
+
+
+def width_percent_to_x(scene_width, width_percent):
+    return - scene_width / 2 + width_percent * scene_width
+
+def height_percent_to_y(scene_height, height_percent):
+    return height_percent * scene_height
+
+def diameter_percent_to_length(scene_width, diameter_percent):
+    return diameter_percent * scene_width
+
+scene_width = 256
+scene_height = 256
+
+image = Image.new("RGB", (scene_width, scene_height), "white")
+draw = ImageDraw.Draw(image)
+
+draw_jar(draw, scene_width, scene_height, "jar", "red", 0.5, 0.5, 0.5, 0.25)
+draw_jar(draw, scene_width, scene_height, "jar", "red", 0.5, 0.5, 0.5, 0)
+
+
+image = image.transpose(Image.FLIP_TOP_BOTTOM)
+image.show()
+# draw_scene(scene_width, scene_height)
+
+
 
 def draw_rectangle(draw, scene_width, scene_height, shape, color, diameter, x, y, angle, offset):
 
@@ -151,26 +188,3 @@ def draw_rectangle(draw, scene_width, scene_height, shape, color, diameter, x, y
     rectangle_vertices = [rotated_about(x, y, rectangle_center[0], rectangle_center[1], math.radians(rectangle_angle)) for x,y in rectangle_vertices]
 
     draw.polygon(rectangle_vertices, fill=color)
-
-def width_percent_to_x(scene_width, width_percent):
-    return - scene_width / 2 + width_percent * scene_width
-
-def height_percent_to_y(scene_height, height_percent):
-    return height_percent * scene_height
-
-def diameter_percent_to_length(scene_width, diameter_percent):
-    return diameter_percent * scene_width
-
-scene_width = 256
-scene_height = 256
-
-image = Image.new("RGB", (scene_width, scene_height), "white")
-draw = ImageDraw.Draw(image)
-
-
-draw_jar(draw, scene_width, scene_height, "jar", "red", 0.1, 0.5, 0.5, 0)
-
-
-image = image.transpose(Image.FLIP_TOP_BOTTOM)
-image.show()
-# draw_scene(scene_width, scene_height)
